b52464972db9cc0cc5f9c8887f512bb06f68c493
Ryan B <bombingrun3@gmail.com>
2025-07-06 11:30:34 -0500

    Added initial files

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+app.use(express.json());
+
+// Routes
+const index = require("./routes/index");
+app.use("/api/example", index);
+
+// Root route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
