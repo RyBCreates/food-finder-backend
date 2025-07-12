2deb110636805dbf924d666efa2cc086cab83acc
Ryan B <bombingrun3@gmail.com>
2025-07-11 22:51:36 -0500

    Added getFavorites functionality

--- controllers/favoriteRecipes.js
+++ controllers/favoriteRecipes.js
@@ -1,6 +1,20 @@
 const Favorite = require("../models/FavoriteRecipes");
 const transformSpoonacularRecipe = require("../utils/transformRecipeData");
 
+// get Favorite Recipes
+
+const getFavorites = async (req, res) => {
+  try {
+    const userId = req.query.userId || "user123";
+    const favorites = await Favorite.find({ userId });
+    res.status(200).json(favorites);
+  } catch (err) {
+    console.error("Error fetching favorites:", err);
+    res.status(500).json({ error: "Failed to get favorites" });
+  }
+};
+
+// Add Recipe to your favorites
 const addFavorite = async (req, res) => {
   try {
     const { userId, recipe } = req.body;
@@ -19,4 +33,4 @@ const addFavorite = async (req, res) => {
   }
 };
 
-module.exports = { addFavorite };
+module.exports = { addFavorite, getFavorites };
--- routes/favoriteRecipes.js
+++ routes/favoriteRecipes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { addFavorite } = require("../controllers/favoriteRecipes");
+const { addFavorite, getFavorites } = require("../controllers/favoriteRecipes");
 
+router.get("/", getFavorites);
 router.post("/", addFavorite);
 
 module.exports = router;
